0e4994808f325e98208b1648babb68c729b0a826
bilalmahmood96 <bilalmahmood44@gmail.com>
2019-07-15 14:25:22 +0500

    Add project files.

---
+++ NewWebApi/Controllers/SearchByCategoryController.cs
@@ -0,0 +1,19 @@
+﻿using NewWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NewWebApi.Controllers
+{
+    public class SearchByCategoryController : ApiController
+    {
+        [HttpGet]
+        public List<dataclass> datastatusbyCategories(string Categories)
+        {
+            return DB.myDB.Where(a => a.Categories.Contains(Categories)).ToList();
+        }
+    }
+}
---
+++ NewWebApi/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using NewWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace NewWebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            string textFile = Server.MapPath("/Models/data.json");
+            if (File.Exists(textFile))
+            {
+                // Read entire text file content in one string  
+                string text = File.ReadAllText(textFile);
+                DB.myDB = JsonConvert.DeserializeObject<List<dataclass>>(text);
+                //Console.WriteLine(text);
+            }
+        }
+    }
+}
